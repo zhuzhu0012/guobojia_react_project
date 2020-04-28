b35422605d50f632a2e4477d0647735b794160ec
zhuzhu <1099297432@qq.com>
2020-04-28 21:11:34 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component } from 'react'
+import Admin from './pages/Admin/Admin'
+import Login from './pages/Login/Login'
+import {Switch,Route,Redirect} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route path="/login" component={Login}/>
+        <Route path="/admin" component={Admin}/>
+        <Redirect to="login"/>
+      </Switch>
+    )
+  }
 }
-
-export default App;
